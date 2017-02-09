8f51e18f904443b0ad4489fecdfe8549b66953d9
John Cleary <john@createk.io>
2017-02-09 16:17:10 +0000

    add search and extract duplication

--- lib/rspec_helpers/selectize_helper.rb
+++ lib/rspec_helpers/selectize_helper.rb
@@ -1,11 +1,20 @@
 module RspecHelpers
   module SelectizeHelper
     def selectize_add_item(selector, item_value)
-      page.execute_script("$('#{selector}')[0].selectize.addItem('#{item_value}')")
+      selectize_exec(selector, "addItem('#{item_value}')")
     end
 
     def selectize_remove_item(selector, item_value)
-      page.execute_script("$('#{selector}')[0].selectize.removeItem('#{item_value}')")
+      selectize_exec(selector, "removeItem('#{item_value}')")
+    end
+
+    def selectize_search(selector, search_term)
+      selectize_exec(selector, "onSearchChange('#{search_term}')")
+      selectize_exec(selector, "open()")
+    end
+
+    def selectize_exec(selector, command)
+      page.execute_script("$('#{selector}')[0].selectize.#{command}")
     end
   end
 end
