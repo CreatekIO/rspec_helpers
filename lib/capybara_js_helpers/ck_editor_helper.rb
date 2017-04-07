57b7a587456d39f75bcde32726036d9c670989b1
John Cleary <john@createk.io>
2017-04-07 23:36:43 +0100

    add document ready check

--- lib/capybara_js_helpers/ck_editor_helper.rb
+++ lib/capybara_js_helpers/ck_editor_helper.rb
@@ -3,8 +3,10 @@ module CapybaraJsHelpers
     def ckeditor_fill_in(locator, opts)
       content = opts.fetch(:with).to_json
       page.execute_script <<-SCRIPT
-      CKEDITOR.instances['#{locator}'].setData(#{content});
-      $('textarea##{locator}').text(#{content});
+      $(function() {
+        CKEDITOR.instances['#{locator}'].setData(#{content});
+        $('textarea##{locator}').text(#{content});
+      });
       SCRIPT
     end
   end
